92b4c6a0346ce56f8e3794e8c89b043e3b5f54f3
Luubstar <nbaronariera@gmail.com>
2023-07-14 16:29:53 +0200

    Mejoras de seguridad (Hay que integrar todavía)

--- src/estadisticas/estadisticas.controller.ts
+++ src/estadisticas/estadisticas.controller.ts
@@ -1,48 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, Req, HttpStatus, HttpException } from '@nestjs/common';
+import { Controller, Get, Body, Param, Req, HttpStatus, HttpException } from '@nestjs/common';
 import { EstadisticasService } from './estadisticas.service';
 import { CreateEstadisticaDto } from './dto/create-estadistica.dto';
 import { UpdateEstadisticaDto } from './dto/update-estadistica.dto';
-import { ApiAcceptedResponse, ApiConflictResponse, ApiHideProperty, ApiOkResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags} from '@nestjs/swagger';
 import {Request } from "express";
 import { Estadisticas } from './schemas/estadistica.schema';
-import { UsuarioService } from 'src/usuarios/usuario.service';
 
 @Controller('estadisticas')
 @ApiTags('estadisticas')
 
-//TODO: Debo mejorar esto...
 
 export class EstadisticasController {
   constructor(private readonly estadisticasService: EstadisticasService) {}
-  @Inject(UsuarioService)
-  private readonly usuario;
 
-  async GetIDByLoggin(correo : string, clave : string):Promise<String> {
-    var res = await this.usuario.GetIfLoged(correo, clave);
-    return res;
-  }
-
-  @ApiOperation({summary: "Crea una estadística para un usuario"})
-  @ApiConflictResponse({description: "Si existe un conflicto al crear la estadística", type:String})
-  @ApiUnauthorizedResponse({description:"Si el usuario introducido está activado y existe", type:String})
-  @ApiAcceptedResponse({description:"La estadística creada", type:Estadisticas})
-  @Post(":correo/:clave")
-  async create(@Body() createEstadisticaDto: CreateEstadisticaDto, @Param("correo") correo:string, @Param("clave") clave:string) {
-      var res = await this.GetIDByLoggin(correo, clave);
+  //Server Only!
+  async create(@Body() createEstadisticaDto: CreateEstadisticaDto, Usuario:string) {
       var date = new Date();
-      if (res.length > 0)
+      if (Usuario.length > 0)
       {
-        if (!(await this.estadisticasService.checkIfMultiple(res.toString(), date.getMonth(), date.getFullYear()))){
-        createEstadisticaDto["Usuario"] = res.toString();
-        createEstadisticaDto["Mes"] = date.getMonth();
-        createEstadisticaDto["Año"] = date.getFullYear();
-        throw new HttpException(this.estadisticasService.create(createEstadisticaDto),HttpStatus.ACCEPTED);
+        if (!(await this.estadisticasService.checkIfMultiple(Usuario.toString(), date.getMonth(), date.getFullYear()))){
+          createEstadisticaDto["Usuario"] = Usuario.toString();
+          createEstadisticaDto["Mes"] = date.getMonth();
+          createEstadisticaDto["Año"] = date.getFullYear();
+          throw new HttpException(this.estadisticasService.create(createEstadisticaDto),HttpStatus.ACCEPTED);
         }
-        else{
-          throw new HttpException("Ya existen estadísticas con ese mes y año para el usuario",HttpStatus.CONFLICT)
-        }
+        else{throw new HttpException("Ya existen estadísticas con ese mes y año para el usuario",HttpStatus.CONFLICT)}
       }
-      else {throw new HttpException("Usuario no encontrado",HttpStatus.UNAUTHORIZED)}
+      else {throw new HttpException("Usuario no indicado",HttpStatus.UNAUTHORIZED)}
   }
 
   @ApiOkResponse({description:"Las estadisticas", isArray:true,type:Estadisticas})
@@ -62,33 +46,11 @@ export class EstadisticasController {
   @ApiOkResponse({description:"La estadistica (si la encuentra)", type:Estadisticas})
   @ApiOperation({summary: "Encuentra una estadística dado un usuario, mes y año"})
   @Get(':userID/:month/:year')
-  async GetByUser(@Param('userID') id: string,@Param("month") mes:number, @Param("year") año:number) {
-    throw new HttpException(this.estadisticasService.getFromUser(id,mes,año),HttpStatus.OK);
-  }
+  async GetByUser(@Param('userID') id: string,@Param("month") mes:number, @Param("year") año:number) {throw new HttpException(this.estadisticasService.getFromUser(id,mes,año),HttpStatus.OK);}
 
-  @ApiOperation({summary: "Actualiza una estadistica por ID"})
-  @ApiOkResponse({description:"La estadistica actualizada", type:Estadisticas})
-  @ApiUnauthorizedResponse({description:"Si el usuario introducido está activado y existe", type:String})
-  @ApiHideProperty()
-  @Patch(':correo/:clave/:id')
-  async update(@Param('id') id: string,@Param("correo") correo:string, @Param("clave") clave:string, @Body() updateEstadisticaDto: UpdateEstadisticaDto) {
-    if ((await this.GetIDByLoggin(correo, clave)).length > 0){
-      throw new HttpException(this.estadisticasService.update(id, updateEstadisticaDto),HttpStatus.OK);}
-    else{
-      throw new HttpException("No tienes autorización",HttpStatus.UNAUTHORIZED);
-    }
-  }
+  //Server Only!
+  async update(id: string, @Body() updateEstadisticaDto: UpdateEstadisticaDto) {throw new HttpException(this.estadisticasService.update(id, updateEstadisticaDto),HttpStatus.OK);}
 
-  @ApiOkResponse({description:"La estadistica eliminada", type:Estadisticas})
-  @ApiOperation({summary: "Elimina una estadistica"})
-  @ApiUnauthorizedResponse({description:"Si el usuario introducido está activado y existe", type:String})
-  @ApiHideProperty()
-  @Delete(':correo/:clave/:id')
-  async remove(@Param('id') id: string,@Param("correo") correo:string, @Param("clave") clave:string) {  
-    if ((await this.GetIDByLoggin(correo, clave)).length > 0){
-    throw new HttpException(this.estadisticasService.remove(id),HttpStatus.OK);}
-    else{
-      throw new HttpException("No tienes autorización",HttpStatus.UNAUTHORIZED);
-    }
-  }
+  //Server Only!
+  async remove(id: string) {throw new HttpException(this.estadisticasService.remove(id),HttpStatus.OK);}
 }
