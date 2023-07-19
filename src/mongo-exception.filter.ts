166727bd08752a919935c3a8db6dca19afb915c4
Luubstar <nbaronariera@gmail.com>
2023-07-19 13:00:58 +0200

    Cambiado el mongofilter

--- src/mongo-exception.filter.ts
+++ src/mongo-exception.filter.ts
@@ -1,18 +1,22 @@
-import { ArgumentsHost, Catch, ConflictException, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ConflictException, ExceptionFilter, HttpStatus } from '@nestjs/common';
 import { MongoError } from 'mongodb';
+import {Response} from "express";
 
 @Catch(MongoError)
 export class MongoExceptionFilter implements ExceptionFilter {
   catch(exception: MongoError, host: ArgumentsHost) {
     switch (exception.code) {
       case 11000:
+        let ctx = host.switchToHttp()
+        let response = ctx.getResponse<Response>();
+        response.status(HttpStatus.BAD_REQUEST).send("Documento duplicado")
         return "DUPLICADO";
-        break;
         // duplicate exception
         // do whatever you want here, for instance send error to client
       default:
-        return "ERROR";
-        break;
+        ctx = host.switchToHttp()
+        response = ctx.getResponse<Response>();
+        response.status(HttpStatus.INTERNAL_SERVER_ERROR).send("Error no identificado")
     }
   }
 }
